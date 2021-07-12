5f05ac0358031cee9c42ffb11c8dc27ba5d12457
Jeremy Straus <Jstraws@verizon.net>
2021-07-12 14:36:15 -0400

    Initial Commit

---
+++ src/main/java/com/straus/fullscore/ProjectFullScoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.straus.fullscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFullScoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFullScoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/straus/fullscore/ProjectFullScoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.straus.fullscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFullScoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
